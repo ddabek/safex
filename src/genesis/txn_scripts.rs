a8905c6591dc190149a264492a2d618e0d8f228a
dallyshalla <dallyshalla@gmail.com>
2016-04-13 17:03:39 +0200

    mod

---
+++ src/genesis/txn_scripts.rs
@@ -0,0 +1,22 @@
+//transaction and script usage
+
+
+use bitcoin::blockdata::transaction::{TxIn, TxOut};
+use bitcoin::blockdata::opcodes;
+use bitcoin::blockdata::script::{Script, Builder};
+
+use rustc_serialize::hex::{ToHex, FromHex};
+
+pub fn pubkey_script() {
+	
+}
+
+
+
+
+#[test]
+fn test() {
+
+	pubkey_script();
+
+}
