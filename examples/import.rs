c86d127283f1501dd026806acc9e30fc0ebd7c6e
Dallyshalla <dallyshalla@gmail.com>
2016-06-11 18:14:37 +0200

    import WIF

--- examples/import.rs
+++ examples/import.rs
@@ -10,13 +10,44 @@ use std::io::{BufRead};
 use safex::genesis::key_generation::KeyPair;
 
 fn main() {
-		println!("input your private key");
+
+
+
+		println!("Enter \"1\" for base64 or \"2\" for WIF import");
 		let mut input2 = String::new();
     	let stdin2 = io::stdin();
     	stdin2.lock().read_line(&mut input2).unwrap();
 
     	let trimmed = input2.trim_right_matches("\n");
-		let new_keys = KeyPair::keypair_frombase64(trimmed.to_string());
-		println!("public key {:?}", KeyPair::address_base58(&new_keys.public));
 
+    	let answer_parse = trimmed.parse().ok().expect("invalid entry");
+
+
+    	match answer_parse {
+    		1 => base64(),
+    		2 => base58(),
+    		_ => println!("something is wrong idk what"),
+    	};
+}
+
+fn base64() {
+	println!("input your private key");
+	let mut input2 = String::new();
+    let stdin2 = io::stdin();
+    stdin2.lock().read_line(&mut input2).unwrap();
+
+    let trimmed = input2.trim_right_matches("\n");
+	let new_keys = KeyPair::keypair_frombase64(trimmed.to_string());
+	println!("public key {:?}", KeyPair::address_base58(&new_keys.public));
+}
+
+fn base58() {
+	println!("input your private key");
+	let mut input2 = String::new();
+    let stdin2 = io::stdin();
+    stdin2.lock().read_line(&mut input2).unwrap();
+
+    let trimmed = input2.trim_right_matches("\n");
+	let new_keys = KeyPair::keypair_frombase58wif(trimmed.to_string());
+	println!("public key {:?}", KeyPair::address_base58(&new_keys.public));
 }
